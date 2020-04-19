23cea32a4fc859bd6135d924894badd6da85d50f
Anton Kyrychenko <anton.kyrychenko11@gmail.com>
2020-04-19 23:46:57 +0300

    Moved AngularClient to the  ClientApp folder of WebApi project

--- WebApi/TestTask.WebApi/Startup.cs
+++ WebApi/TestTask.WebApi/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -59,6 +60,16 @@ namespace TestTask.WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
             });
         }
     }
